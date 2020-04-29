d540bdf604991baf5055ebadd289866d8112d544
Frederic FOISSEY <frederic.foissey@gmail.com>
2020-04-29 13:30:22 +0200

    [DEV] modif parser + blt prototype

---
+++ srcs/blt.c
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   blt.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffoissey <ffoissey@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/29 13:08:27 by ffoissey          #+#    #+#             */
+/*   Updated: 2020/04/29 13:14:17 by ffoissey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "client_taskmaster.h"
+
+t_cmd	*blt_add(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_avail(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_clear(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_exit(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_fg(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_maintail(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_open(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_pid(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_quit(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_reload(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_remove(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_reread(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_restart(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_shutdown(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_signal(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_start(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_status(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_stop(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_tail(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_update(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
+t_cmd	*blt_version(t_vector *arg)
+{
+	(void)arg;
+	return (NULL);
+}
+
