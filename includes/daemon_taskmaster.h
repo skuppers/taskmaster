32933eb9eca62fa05a32db9832ad731aec61ecd3
seb <sebastien.kuppers@gmail.com>
2020-04-30 10:18:43 +0200

    daemon init

---
+++ includes/daemon_taskmaster.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   daemon_taskmaster.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffoissey <ffoissey@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/29 11:36:21 by ffoissey          #+#    #+#             */
+/*   Updated: 2020/04/29 20:22:56 by ffoissey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DAEMON_TASKMASTER_H
+# define DAEMON_TASKMASTER_H
+
+# include <unistd.h>
+# include <sys/socket.h>
+# include <sys/un.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+
+# include "libft.h"
+# include "stdint.h"
+
+# define DFLT_SOCKET	"/tmp/taskmstsock"
+
+# endif
