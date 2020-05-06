e55da66b7c1cf6e97d7b3ed1e8c9af01a0dba5a2
seb <sebastien.kuppers@gmail.com>
2020-05-06 16:48:28 +0200

    [Dev] unlinkind socket

--- srcs/daemon/sockets.c
+++ srcs/daemon/sockets.c
@@ -31,8 +31,18 @@ int8_t		bind_socket(t_denv *env)
 {
 	if (bind(env->unix_socket, (struct sockaddr*)&env->addr, sizeof(env->addr)) == FAILURE)
 	{
-		tlog(E_LOGLVL_CRIT, "Bind() failed - %s. Is another daemon running?\n", strerror(errno));
-        exit_routine();
+		if (errno != EADDRINUSE)
+		{
+			tlog(E_LOGLVL_CRIT, "Bind() failed - %s. Is another daemon running?\n", strerror(errno));
+        	exit_routine();
+		}
+		tlog(E_LOGLVL_WARN, "Unlinking existing socket: %s\n", env->addr.sun_path);
+		unlink(DFL_SOCKET);
+		if (bind(env->unix_socket, (struct sockaddr*)&env->addr, sizeof(env->addr)) == FAILURE)
+		{
+			tlog(E_LOGLVL_CRIT, "Bind() failed - %s. Is another daemon running?\n", strerror(errno));
+        	exit_routine();
+		}
 	}
   	if (listen(env->unix_socket, MAX_CLIENTS) == -1)
 	{
