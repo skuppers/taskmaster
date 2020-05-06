a4a36b35fbed53bb67b3971735cf405cb7eeb610
Frederic FOISSEY <frederic.foissey@gmail.com>
2020-05-06 16:29:37 +0200

    [FIX]

--- srcs/daemon/opt_d.c
+++ srcs/daemon/opt_d.c
@@ -6,7 +6,7 @@
 /*   By: ffoissey <ffoissey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 13:21:56 by ffoissey          #+#    #+#             */
-/*   Updated: 2020/05/06 16:27:04 by ffoissey         ###   ########.fr       */
+/*   Updated: 2020/05/06 16:29:25 by ffoissey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ static int		parse_opt(char **av, int ac, int i)
 		if (ft_strequ(av[i], opt_str[count]) == TRUE)
 		{
 			g_denv->opt.optmask |= (1 << (count / 2));
-			if (count >= 8) // needs arguments
+			if (count >= 6) // needs arguments
 			{
 				i++;
 				if (i == ac || av[i][0] == '-')
