99ec588767f485ecfd663e46aca62178f4f81982
Frederic FOISSEY <frederic.foissey@gmail.com>
2020-05-12 15:14:53 +0200

    [DEV] daemon: upate cmd struct / remove cmd struct

--- srcs/daemon/builtin/cmd_remove.c
+++ srcs/daemon/builtin/cmd_remove.c
@@ -6,14 +6,19 @@
 /*   By: ffoissey <ffoissey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 02:32:31 by ffoissey          #+#    #+#             */
-/*   Updated: 2020/05/05 02:32:42 by ffoissey         ###   ########.fr       */
+/*   Updated: 2020/05/12 15:12:01 by ffoissey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "daemon_taskmaster.h"
 
+t_vector	*action_remove(t_instance *instance, t_program *program)
+{
+	(void)instance;	
+	return (get_msg(program->name, "process group still running", ERR_MSG));
+}
+
 t_vector			*cmd_remove(t_cmd *cmd)
 {
-	(void)cmd;
-	return (NULL);
+	return (exec_action_args_group(cmd->av, cmd->ac, action_remove));
 }
--- srcs/daemon/builtin/cmd_update.c
+++ srcs/daemon/builtin/cmd_update.c
@@ -6,7 +6,7 @@
 /*   By: ffoissey <ffoissey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 02:33:41 by ffoissey          #+#    #+#             */
-/*   Updated: 2020/05/05 02:33:52 by ffoissey         ###   ########.fr       */
+/*   Updated: 2020/05/12 15:14:29 by ffoissey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,15 @@ t_vector		*update_file(t_instance *in, t_program *prg)
 	return (vct_newstr("Updated config file.\n"));
 }
 
+t_vector	*action_update(t_instance *instance, t_program *program)
+{
+	(void)instance;	
+	return (get_msg(program->name, "unknown error", ERR_MSG));
+}
+
 t_vector			*cmd_update(t_cmd *cmd)
 {
-	t_vector	*vct;
-
-	vct = NULL;
 	if (cmd->ocp == 0x01 || cmd->ocp == 0x02)
-		vct = update_file(NULL, NULL);
-	return (vct);
+		return (update_file(NULL, NULL)); // return (exec_action_all_groups(action_update));
+	return (exec_action_args_group(cmd->av, cmd->ac, action_update));
 }
