82974dd39fbda371daef9c667b449125b874c9e5
seb <sebastien.kuppers@gmail.com>
2020-05-12 10:55:12 +0200

    [DEV] reread

--- srcs/daemon/builtin/cmd_reread.c
+++ srcs/daemon/builtin/cmd_reread.c
@@ -12,6 +12,28 @@
 
 #include "daemon_taskmaster.h"
 
+static uint8_t		reread_file(void)
+{
+	dictionary	*dict;
+	int			sections;
+
+	dict = iniparser_load(g_denv->opt.str[CONFIGURATION]);
+	sections = iniparser_getnsec(dict);
+	while (sections >= 0)
+	{
+		if (ft_strequ(iniparser_getsecname(g_denv->dict, sections),
+					"taskmasterd") == true)
+		{
+			taskmasterd_override(g_denv, g_denv->dict);
+			return ;
+		}
+		--sections;
+	}
+	if (sections < 0)
+		dprintf(STDERR_FILENO, "taskmasterd: [taskmasterd]"
+								" section is not defined.\n");
+}
+
 t_vector			*cmd_reread(t_cmd *cmd)
 {
 	(void)cmd;
