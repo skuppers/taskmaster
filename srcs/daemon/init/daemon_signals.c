9ea3b1b7c374ab2aca909444f2afa42a5e39b918
Frederic FOISSEY <frederic.foissey@gmail.com>
2020-05-13 18:29:32 +0200

    [DEV] SIGHUP

--- srcs/daemon/init/daemon_signals.c
+++ srcs/daemon/init/daemon_signals.c
@@ -6,7 +6,7 @@
 /*   By: ffoissey <ffoisssey@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 14:13:28 by ffoissey          #+#    #+#             */
-/*   Updated: 2020/05/13 17:24:38 by ffoissey         ###   ########.fr       */
+/*   Updated: 2020/05/13 18:25:27 by ffoissey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,19 @@
 
 void	sigall_handle(int signo)
 {
-	exit_routine(E_LOGLVL_CRIT, strsignal(signo));
+	exit_routine(E_LOGLVL_INFO, strsignal(signo));
+}
+
+void	sighup_handle(int signo)
+{
+	tlog(E_LOGLVL_INFO, "%s\n", strsignal(signo));
+	reread_file(NULL, NULL);
 }
 
 void	init_signals(void)
 {
 	struct sigaction	sig_all;
+	struct sigaction	sig_hup;
 
 	sigemptyset(&sig_all.sa_mask);
 	sig_all.sa_handler = sigall_handle;
@@ -30,4 +37,8 @@ void	init_signals(void)
 	sigaction(SIGUSR2, &sig_all, NULL);
 	sigaction(SIGPIPE, &sig_all, NULL);
 	sigaction(SIGINT, &sig_all, NULL);
+	sigemptyset(&sig_hup.sa_mask);
+	sig_hup.sa_handler = sighup_handle;
+	sig_hup.sa_flags = SA_RESTART;
+	sigaction(SIGHUP, &sig_hup, NULL);
 }
