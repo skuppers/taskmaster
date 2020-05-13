706a4dcbc151ae1b227e9a51d58823213517feec
Frederic FOISSEY <frederic.foissey@gmail.com>
2020-05-13 11:08:24 +0200

    [fix] tail (bytes)

--- srcs/daemon/builtin/cmd_maintail.c
+++ srcs/daemon/builtin/cmd_maintail.c
@@ -6,7 +6,7 @@
 /*   By: ffoissey <ffoissey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 02:27:30 by ffoissey          #+#    #+#             */
-/*   Updated: 2020/05/12 23:29:20 by ffoissey         ###   ########.fr       */
+/*   Updated: 2020/05/13 11:07:46 by ffoissey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ t_vector			*cmd_maintail(t_cmd *cmd)
 	(void)cmd;
 	if (g_denv->opt.str[LOGFILE] == NULL)
 		return (get_msg("taskmasterd", "no log file", ERR_MSG));
+	else if (g_denv->opt.optmask & OPT_NODAEMON)
+		return (get_msg("taskmasterd", "no log file [nodaemon mode]", ERR_MSG));
 	vct = vct_newstr(g_denv->opt.str[LOGFILE]);
 	vct_add(vct, ETX);
 	return (vct);
